ce148c845e5bf2e2df70fc29036872f564667803
kosukeyokoyama <yokoyoko.yobi@gmail.com>
2026-01-22 19:38:28 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,62 @@
 class Test {
-
-  constructor() {}
-
-  getInfo() { // 拡張機能の各種情報
-    return {
-      id: 'test',
-      name: 'Test', // 拡張機能の名前
-      blocks: [ // 各ブロックの定義
-        {
-          opcode: 'hello', // このブロックが実行されると、helloという関数が呼ばれる
-          blockType: Scratch.BlockType.COMMAND,　// 「10歩動かす」のような通常の命令ブロック
-          text: 'hello' // ブロックに表示されるテキスト
-        }
-      ]
-    }
+  constructor() {
+    // データを保存するためのメモリ代わりのオブジェクト
+    // 本来は外部DBとかを使いたいところだが、まずはローカル変数で管理だ！
+    this.storage = {};
   }
 
-  hello() {
-    console.log('hello'); // console log に hello と出力
+  getInfo() {
+    return {
+      id: 'test',
+      name: 'Data Logger Extension',
+      blocks: [
+        {
+          opcode: 'manageData', // 実行される関数名
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'データを [MODE] する (ID: [ID]) 内容: [DATA]',
+          arguments: {
+            MODE: {
+              type: Scratch.ArgumentType.STRING,
+              menu: 'modeMenu', // 下で定義するメニューを使用
+              defaultValue: 'write'
+            },
+            ID: {
+              type: Scratch.ArgumentType.STRING, // 識別番号
+              defaultValue: '001'
+            },
+            DATA: {
+              type: Scratch.ArgumentType.STRING, // 記録するデータ
+              defaultValue: 'some data'
+            }
+          }
+        }
+      ],
+      menus: {
+        modeMenu: {
+          acceptReporters: false,
+          items: ['write', 'read'] // ここで選択肢を固定
+        }
+      }
+    };
+  }
+
+  // ブロックが実行された時の処理
+  manageData(args) {
+    const mode = args.MODE;
+    const id = args.ID;
+    const data = args.DATA;
+
+    if (mode === 'write') {
+      // データの書き込み処理
+      this.storage[id] = data;
+      console.log(`[Save Success] ID: ${id} にデータを書き込んだぜ: ${data}`);
+    } else if (mode === 'read') {
+      // データの読み出し処理
+      const result = this.storage[id] || "データが見つからねぇな...";
+      console.log(`[Read Success] ID: ${id} のデータはこれだ: ${result}`);
+      // 本来は「値ブロック」にしてScratch側に返したいところだが、
+      // 今回はCOMMAND型なのでコンソール出力にとどめてるぜ！
+    }
   }
 }
 
